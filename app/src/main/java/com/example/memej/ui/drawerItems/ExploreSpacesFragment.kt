ce42d917dc6781b879031f8d916a32d2c43a064c
Kavya-24 <Kavya_1901cs30@iitp.ac.in>
2020-07-22 16:58:39 +0530

    Update on how FAB works in Drawer elements

--- app/src/main/java/com/example/memej/ui/drawerItems/ExploreSpacesFragment.kt
+++ app/src/main/java/com/example/memej/ui/drawerItems/ExploreSpacesFragment.kt
@@ -4,12 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.memej.R
 import com.example.memej.viewModels.ExploreSpacesViewModel
-import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class ExploreSpacesFragment : Fragment() {
 
@@ -28,13 +26,13 @@ class ExploreSpacesFragment : Fragment() {
 
         root = inflater.inflate(R.layout.explore_spaces_fragment, container, false)
 
-        //Init the recycler view
-        //Add notation for Observe List from the Workspace Tags
 
-        val fab = root.findViewById<FloatingActionButton>(R.id.fab_workspace)
-        fab.setOnClickListener {
-            Toast.makeText(requireContext(), "OKAJus", Toast.LENGTH_SHORT).show()
-        }
+
+
+
+
+
+
         return root
     }
 
